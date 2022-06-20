1f9577c2389a4a15116c60b5046c1f2d55de8ab9
Khesav Chhatra <shahkhesavchhatra@gmail.com>
2022-06-20 06:31:30 +0545

    first commit EBO VBO classes

--- main.cpp
+++ main.cpp
@@ -1,8 +1,34 @@
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+#include"ShaderClass.h"
+#include"VAO.h"
+#include"VBO.h"
+#include"EBO.h"
 //let's initialize a pointer to window
 GLFWwindow* window;
+GLfloat vertices[] = {
+    -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
+        0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
+        0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
+        -0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
+        0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
+        0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
+
+};
+
+GLuint indices[] =
+{
+    0, 3, 5, // Lower left triangle
+    3, 2, 4, // Lower right triangle
+    5, 4, 1 // Upper triangle
+};
+
+
+
+
+
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
@@ -33,14 +59,33 @@ void processInput(GLFWwindow* window)
 
 int main() {
     InitializeWindow();
+    
+    
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
     glfwMakeContextCurrent(window);
+    
     glViewport(0, 0, 800, 600);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    //---------vertex shader---------//
+    //generate shader object using default.vert and default.frag file
+    Shader shaderProgram("default.vert", "default.frag");
+    //Generates vertex array object
+    VAO VAO1;
+    VAO1.Bind();
+    //generates VBO and EBO 
+
+    VBO VBO1(vertices, sizeof(vertices));
+    EBO EBO1(indices, sizeof(indices));
+    //Links VBO to VAO
+    VAO1.LinkVBO(VBO1, 0);
+    //unbind all object to prevent accidental modification
+    VAO1.Unbind();
+    VBO1.Unbind();
+    EBO1.Unbind();
+
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
@@ -48,9 +93,25 @@ int main() {
         //...
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        //glUseProgram(shaderProgram);
+        shaderProgram.Activate();//to activate shader program
+        VAO1.Bind();
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, sizeof(vertices)/sizeof(float), GL_UNSIGNED_INT, 0);
         // check and call events and swap the buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+   
+    //glDeleteProgram(shaderProgram);delete the shader program
+    VAO1.Delete();
+    VBO1.Delete();
+    EBO1.Delete();
+    shaderProgram.Delete();
+    //delete the window
+    glfwDestroyWindow(window);
+    //terminate
+    glfwTerminate();
 	return 0;
 }
